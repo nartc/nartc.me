de9623e5ef519ab4c814a32efc0e54a053b35082
Chau Tran <chautran@10.0.0.12>
2022-08-13 14:11:47 +0700

    setup tailwind and astro with theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
+const { fontFamily, spacing } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
-        extend: {},
+        fontFamily: {
+            sans: ["Source Sans Pro", ...fontFamily.sans],
+        },
+        extend: {
+            colors: {
+                primary: "var(--primary)",
+                secondary: "var(--secondary)",
+                "gray-light": "var(--gray-light)",
+                "gray-medium": "var(--gray-medium)",
+                "gray-dark": "var(--gray-dark)",
+                text: "var(--text-color)",
+            },
+        },
     },
     plugins: [],
 };
