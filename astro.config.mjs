09928ef31af2f27deb1aed0c8c3199a6a59e6db4
Chau Tran <chautran@10.0.0.11>
2022-08-23 19:20:42 +0700

    shared module

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
 import image from "@astrojs/image";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), image()],
-    site: "https://nartc.me",
-    markdown: {
-        shikiConfig: {
-            theme: "dark-plus",
-        },
-    },
+  integrations: [tailwind(), image(), mdx()],
+  site: "https://nartc.me",
+  markdown: {
+    shikiConfig: {
+      theme: "dark-plus"
+    }
+  }
 });
