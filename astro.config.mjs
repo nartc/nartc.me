9dbd9bb2bc1cd0a879a15463e439c5bbbe7138b5
Chau Tran <chautran@10.0.0.7>
2022-10-18 22:14:56 +0700

    Revert "convert components to angular"
    This reverts commit 8fefdcf674b7a497d49e787baae2c72264f62811.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,16 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
+
 import analogjsangular from "@analogjs/astro-angular";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), image(), mdx(), analogjsangular()],
-    site: "https://nartc.me",
-    markdown: {
-        shikiConfig: {
-            theme: "dark-plus",
-        },
-    },
+  integrations: [tailwind(), image(), mdx(), analogjsangular()],
+  site: "https://nartc.me",
+  markdown: {
+    shikiConfig: {
+      theme: "dark-plus"
+    }
+  }
 });
