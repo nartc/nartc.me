66f413a9dcb886651d98973e947618f052702a84
Chau Tran <chautran@Chaus-MacBook-Pro.local>
2022-11-02 11:38:03 -0400

    add analog

---
+++ src/components/copyright.component.ts
@@ -0,0 +1,27 @@
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+
+@Component({
+  selector: 'copyright',
+  standalone: true,
+  template: `
+    <p class="text-gray-medium font-thin text-sm">
+      © All rights reserved. AngularVietnam
+    </p>
+    <p class="text-gray-medium font-thin text-sm ml-1 mt-0 lg:mt-2 lg:ml-0">
+      Built with
+      <a
+        href="https://astro.build"
+        rel="noreferrer"
+        target="_blank"
+        class="cursor-pointer text-primary hover:underline"
+      >
+        Astro
+      </a>
+    </p>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    'class': 'flex flex-row lg:flex-col'
+  }
+})
+export class Copyright {}
