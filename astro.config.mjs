ed4674cdeab7025c26d19d1bfb350719fc41d95c
Chau Tran <nartc7789@gmail.com>
2023-08-30 12:36:37 -0500

    migrate 3.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import image from "@astrojs/image";
 
 import mdx from "@astrojs/mdx";
 import rehypePrism from "rehype-prism-plus";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), image(), mdx()],
+    integrations: [tailwind(), mdx()],
     site: "https://nartc.me/",
     markdown: {
         syntaxHighlight: false,
