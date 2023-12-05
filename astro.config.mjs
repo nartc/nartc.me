435dc46b5dbb1b2faa0b0623354027e685ac0131
Chau Tran <nartc7789@gmail.com>
2023-12-05 12:55:07 -0600

    setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
+import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
-import mdx from "@astrojs/mdx";
+import rehypePrism from "rehype-prism-plus";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), mdx()],
 	site: "https://nartc.me/",
+	markdown: {
+		syntaxHighlight: "prism",
+		rehypePlugins: [[rehypePrism, { showLineNumbers: true }]],
+		remarkPlugins: [],
+	},
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,29 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		fontFamily: {
+			sans: ["Inter", ...fontFamily.sans],
+		},
+		extend: {
+			minHeight: {
+				128: "32rem",
+			},
+			boxShadow: {
+				common: "var(--shadow)",
+			},
+			colors: {
+				primary: "var(--primary)",
+				secondary: "var(--secondary)",
+				"gray-light": "var(--gray-light)",
+				"gray-medium": "var(--gray-medium)",
+				"gray-dark": "var(--gray-dark)",
+				text: "var(--text-color)",
+				"code-background": "var(--code-background)",
+			},
+		},
 	},
 	plugins: [],
 };
